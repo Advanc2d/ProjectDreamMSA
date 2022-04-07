c15b2697f2f11515d828d8cabf119c7dbf5e9a89
[Github <0406jhp@naver.com>
2022-04-07 16:32:09 +0900

    [박준호] 04-07 16:31 order-service 추가 + main-service 뷰 고친거 올림.

---
+++ order-service/src/main/java/com/dream/orderService/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.dream.orderService.service;
+
+import com.dream.orderService.domain.OrderProductVO;
+
+public interface OrderService {
+	
+	public OrderProductVO bringLoan(int proNo);
+}
---
+++ order-service/src/main/java/com/dream/orderService/service/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package com.dream.orderService.service;
+
+import org.springframework.stereotype.Service;
+
+import com.dream.orderService.domain.OrderProductVO;
+import com.dream.orderService.mapper.OrderMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+	
+	private final OrderMapper mapper;
+	
+	@Override
+	public OrderProductVO bringLoan(int proNo) {
+
+		log.info("----------------------loan-service mapper.xml 작동-----------------------");
+		return mapper.bringLoan(proNo);
+	}
+	
+}
